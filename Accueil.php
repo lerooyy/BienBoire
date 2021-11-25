2d9c61691333ea9a0957bce133a6e7e234dc6a2d
arthur <leroy1tramp@gmail.com>
2021-11-25 11:54:15 +0100

    2

---
+++ Accueil.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Bien Boire</title>
+  <link rel="stylesheet" href="Accueil.css">
+  <script></script>
+</head>
+    <header>
+        <h1>Bien Boire</h1>
+        <nav>
+            <ul>
+                <li><a href="&"> Recettes <a></li>
+                <li><a href="&"> Aliments <a></li>
+                <li><a href="&"> Panier <a></li>
+                <li><a href="&"> Compte <a></li>
+            </ul>
+        </nav>
+    </header>
+    <body>
+        <nav>
+            <p>ezjfhzeifhzeifhzeifhzeiouzehdizehiouzehfoiuzhIUHFUEZHGZfuizeGFYUZEGFuyigUFGZHFBEZJHFBZEJBFZEKJBFKZBFZJ</p>
+        </nav>
+        <section>
+            <p>ezjfhzeifhzeifhzeifhzeiouzehdizehiouzehfoiuzhIUHFUEZHGZfuizeGFYUZEGFuyigUFGZHFBEZJHFBZEJBFZEKJBFKZBFZJ</p>
+        </section>
+    </body>
+
+</html>
