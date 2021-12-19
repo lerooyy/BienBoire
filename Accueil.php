283d477084ec45c36bddd9e6993e54ecedcd0f63
Sacha <gorse.sacha@gmail.com>
2021-12-19 21:20:06 +0100

    Premier menu Aliment qui est maintenant animé

--- Accueil.php
+++ Accueil.php
@@ -4,27 +4,70 @@
   <meta charset="utf-8">
   <title>Bien Boire</title>
   <link rel="stylesheet" href="Accueil.css">
-  <script></script>
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script type="text/javascript">
+    function toggleMenuAliments() {
+        var menu = document.querySelector(".menuAliments");
+        if (menu.classList.contains("invisible")) {
+            menu.classList.remove("invisible");
+        } else {
+            menu.classList.add("invisible");
+        }
+
+        var container = document.querySelector(".contenuPrincipal");
+        if (container.classList.contains("menu-visible")) {
+            container.classList.remove("menu-visible");
+        } else {
+            container.classList.add("menu-visible");
+        }
+    }
+
+    </script>
+  <?php include 'Donnees.inc.php' ?>
 </head>
 <body>
     <header>
         <h1>Bien Boire</h1>
-        <nav>
+        <nav class="menuPrincipal">
             <ul>
+                <li><div class="openMenu"> Aliments </div></li>
                 <li><a href="#"> Recettes <a></li>
-                <li><a href="#"> Aliments <a></li>
                 <li><a href="#"> Panier <a></li>
                 <li><a href="#"> Compte <a></li>
             </ul>
         </nav>
     </header>
+    <nav class="menuAliments">
+        <?php
+        $tabAliments = array(); 
+        foreach ($Hierarchie as $key => $value) {
+            if ($key == 'Aliment') {
+                foreach ($value as $k => $v) {
+                    if ($k == 'sous-categorie') {
+                        $tabAliments = $v;
+                    }
+                }
+            }
+        }
 
-    <div class="centre">
-        <nav id="menuAliments">
-            <p>ezjfhzeifhzJ</p>
-        </nav>
+        $menuHTML = '<ul>';
+        foreach($tabAliments as $value) {
+            $menuHTML = $menuHTML.'<li><a href="#">'.$value.'<a></li>';
+        }
+
+        $menuHTML = $menuHTML.'</ul>';
+
+        echo $menuHTML;
+        ?>
+    </nav>
+    <script type="text/javascript">
+        document.querySelector(".openMenu").addEventListener('click', (event) => {
+                toggleMenuAliments();
+        }, false);
+    </script>
+    <div class="contenuPrincipal">
         <section>
-            <p>ezjfhzeifhze</p>
+            <?php echo "<p>OKfff</p>"  ?>
         </section>
     </div>
 </body>
