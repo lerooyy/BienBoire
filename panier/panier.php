41075b147f8b332a06fda5b3fdbfecf872f7bc33
arthur <leroy1tramp@gmail.com>
2021-12-22 12:09:51 +0100

    Ajustement affichage et css pour le panier

--- panier/panier.php
+++ panier/panier.php
@@ -10,13 +10,15 @@
 <body>
 <h1>Mes recettes</h1>
     <div class="contenant">
-        <?php //récupérer recettes dans les cookies
-            //echo $_COOKIE['recette'];
+        <?php 
+            /**
+             * Code html pour afficher les recettes favorites dans le panier
+             */
             $recette;
             foreach($_COOKIE as $key => $value){
                 $recetteFavorite = str_replace("_"," ", $value);
                 $recette = $recette.'<div class="recetteFavorite">';
-                $recette = $recette.'<button onclick=supprimerRecette("'.$key.'")>Supprimer du panier</button>';
+                $recette = $recette.'<button onclick=supprimerRecette("'.$key.'") id="supprimerFavoris">Supprimer du panier</button>';
                 $recette = $recette.$recetteFavorite;
                 $recette = $recette.'</div>';
                 $recette = $recette.'<br>';
